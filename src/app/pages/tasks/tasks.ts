d625cc572a38590bdb9e05ede91873152ecb6048
Thiago Gabriel <thiagogab.20@gmail.com>
2026-02-04 22:02:34 -0300

    feat: criação de tasks

--- src/app/pages/tasks/tasks.ts
+++ src/app/pages/tasks/tasks.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 import { TasksService } from '../../services/TasksService';
 import { Task } from '../../models/tasks';
 
@@ -10,19 +12,34 @@ import { Task } from '../../models/tasks';
   selector: 'app-tasks',
   standalone: true,
   imports: [
-    CommonModule, 
-    FormsModule, 
-    MatCardModule, 
-    MatCheckboxModule
+    CommonModule,
+    FormsModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatButtonModule
   ],
-
   templateUrl: './tasks.html',
-  styleUrl: './tasks.scss',
+  styleUrls: ['./tasks.scss']
 })
 
 export class Tasks {
   tasks: Task[] = [];
+  newTaskTitle: string = '';
+
   constructor(private tasksService: TasksService) {
     this.tasks = this.tasksService.getTasks();
+  }
+
+  addTask(): void {
+    if (this.newTaskTitle.trim()) {
+
+      this.tasksService.addTask(this.newTaskTitle.trim());
+      this.newTaskTitle = '';
+    }
+  }
+
+  get completedTasks(): number {
+    return this.tasks.filter(task => task.done).length;
   }
 }
--- src/app/services/TasksService.ts
+++ src/app/services/TasksService.ts
@@ -5,17 +5,42 @@ import { Task } from "../models/tasks";
     providedIn: 'root'
 })
 export class TasksService {
+    private storageKey = 'tasks';
+
     private tasks: Task[] = [
         { id: 1, title: 'Estudar Angular', done: false },
         { id: 2, title: 'Criar dashboard', done: true },
         { id: 3, title: 'Subir projeto no GitHub', done: false }
     ];
 
+    constructor() {
+        const savedTasks = localStorage.getItem(this.storageKey);
+        if (savedTasks) {
+            this.tasks = JSON.parse(savedTasks);
+        }
+    }
+
     getTasks(): Task[] {
         return this.tasks;
     }
 
+    addTask(title: string): void {
+        const newTask: Task = {
+            id: Date.now(),
+            title,
+            done: false
+        };
+        this.tasks.push(newTask);
+        this.save();
+    }
+
     toggleTask(task: Task): void {
         task.done = !task.done;
+        this.save();
     }   
+
+    private save(): void {
+        localStorage.setItem(this.storageKey, JSON.stringify(this.tasks));
+    }
+
 }
