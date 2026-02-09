c4f7732fe62d7563a9e67ca98fef53f09eb60383
Thiago Gabriel <thiagogab.20@gmail.com>
2026-02-09 17:49:15 -0300

    feat: novo style para as tasks pendentes na page Dashboard

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { TasksService } from '../../services/TasksService';
+import { Task } from '../../models/tasks';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
+  imports: [CommonModule, MatProgressBarModule, MatListModule]
 })
-export class Dashboard {
+export class Dashboard implements OnInit {
 
+  total = 0;
+  done = 0;
+  progress = 0;
+  top3pending: Task[] = [];
+
+  constructor(private tasksService: TasksService) { }
+
+  ngOnInit(): void {
+    this.calculateStats();
+  }
+
+  private calculateStats(): void {
+    const tasks = this.tasksService.getTasks();
+
+    this.total = tasks.length;
+    this.done = tasks.filter(t => t.done).length;
+
+    this.progress = this.total === 0
+      ? 0
+      : Math.round((this.done / this.total) * 100);
+
+    this.top3pending = tasks
+      .filter(t => !t.done)
+      .slice(0, 3);
+  }
 }
